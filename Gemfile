64d9ecd5a2442a511ab3cf8127568661e4398197
Marlon Santos Constante <marlon.constante@gmail.com>
2014-03-11 15:22:07 -0300

    Adicionando orchestrate.io

--- Gemfile
+++ Gemfile
@@ -3,6 +3,7 @@ source "https://rubygems.org"
 ruby "2.1.1"
 
 gem "sinatra", "~> 1.4.4"
+gem "orchestrate.io"
 gem "thin"
 
 # gem "rails"
--- service.rb
+++ service.rb
@@ -1,5 +1,13 @@
 require "sinatra"
+require "orchestrate.io"
+require "json"
 
-get "/" do
-  "Hello, world"
+get "/parceiro/pesquisa/*" do |query|
+  @io = OrchestrateIo.new(api_key: "f90dde76-cee4-4524-80f7-77493b6d1f1f:")
+
+  @io.search :get do
+    collection "Parceiro"
+    query       query
+  end.perform
+
 end
