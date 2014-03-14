e172979d50b2d3a2b1534a8a0c4cd7cd1c406c6f
Marlon Santos Constante <marlon.constante@gmail.com>
2014-03-14 14:28:25 -0300

    Ajustando comportamentos de tela

--- src/scripts/javascript/experiencias.js
+++ src/scripts/javascript/experiencias.js
@@ -1,43 +1,20 @@
-var values = [
-    {
-        "codigo": "teste",
-        "descricao": "RAFTING AVENTURA",
-        "local": "Rua Carlos Gomes, 190 - Centro - Três Coroas - RS",
-        "preco": "R$ 525,00",
-        "parceiro": {
-            "nome": "Ecoaventuras",
-            "contatos": [
-                {
-                    "nome": "Édrei",
-                    "telefone": "(51) 3546-4466"
-                }
-            ]
-        },
-        termos: [
-            {
-                "descricao": "Tudo que se consumir no parque das laranjeiras só poderá ser pago em dinheiro ou cheque. O local não aceita nenhum tipo de cartão"
-            }
-        ]
-    }
-];
-
 var domain = {
-    codigo: ko.observable(),
-    descricao: ko.observable(),
-    local: ko.observable(),
-    preco: ko.observable(),
+    codigo: ko.observable(""),
+    descricao: ko.observable(""),
+    local: ko.observable(""),
+    preco: ko.observable(""),
     parceiro: {
-        nome: ko.observable(),
+        nome: ko.observable(""),
         contatos: ko.observableArray([
             {
-                nome: ko.observable(),
-                telefone: ko.observable()
+                nome: ko.observable(""),
+                telefone: ko.observable("")
             }
         ])
     },
     termos: ko.observableArray([
         {
-            descricao: ko.observable()
+            descricao: ko.observable("")
         }
     ])
 };
@@ -55,9 +32,7 @@ var model = {
                 data = JSON.parse(data);
                 var array = [];
                 for (var index = 0; index < data.count; index++) {
-                    var value = data.results[index].value;
-                    value.codigo = data.results[index].path.key;
-                    array.push(value);
+                    array.push(data.results[index].value);
                 }
                 model.values(array);
                 model.clear();
@@ -65,11 +40,9 @@ var model = {
     },
     save: function () {
         var value = ko.toJS(model.selected);
-        var codigo = value.codigo;
-        delete value.codigo;
         $.ajax({
             type: "POST",
-            url: "http://localhost:5000/experiencia/salvar/" + codigo,
+            url: "http://localhost:5000/experiencia/salvar/" + value.codigo,
             data: JSON.stringify(value)
         })
             .done(function () {
@@ -81,11 +54,9 @@ var model = {
     },
     exclude: function () {
         var value = ko.toJS(model.selected);
-        var codigo = value.codigo;
-        delete value.codigo;
         $.ajax({
             type: "DELETE",
-            url: "http://localhost:5000/experiencia/remover/" + codigo
+            url: "http://localhost:5000/experiencia/remover/" + value.codigo
         })
             .done(function () {
                 model.find();
