93d7b9aba238910615aacbc112cfe831044cadba
Marlon Santos Constante <marlon.constante@gmail.com>
2014-03-18 15:13:17 -0300

    Atualizando layout do voucher

--- src/ruby/voucher.rb
+++ src/ruby/voucher.rb
@@ -4,12 +4,31 @@ require "launchy"
 class Voucher
 
   def initialize
-    @pdf = PDFKit.new(File.new("src/views/templates/voucher.html"))
+    pattern = "{{.*?}}"
+    params = {"{{EXPERIENCIA}}" => "RAFTING AVENTURA",
+              "{{CLIENTE}}" => "Patricia Tissiani",
+              "{{PARCEIRO}}" => "Ecoaventuras",
+              "{{ENDERECO}}" => "Rua Carlos Gomes, 190 - Centro - Tr&ecirc;s Coroas - RS",
+              "{{CONTATOS}}" => "Édrei -  (51) 3546-4466",
+              "{{VALOR_PAGO}}" => "R$ 525,00",
+              "{{NUMERO_VOUCHER}}" => "862081CB-65BC-463A-A50A-170664DE2675"}
+
+    @template = "src/views/templates/voucher.html"
+    @outputHtml = "src/views/templates/voucher-#{params['{{NUMERO_VOUCHER}}']}.html"
+    @outputPdf = "files/voucher-#{params['{{NUMERO_VOUCHER}}']}.pdf"
+
+    File.open(@outputHtml, 'w') do |out|
+      out << File.open(@template).read.gsub(/#{pattern}/, params)
+    end
+
+    @pdf = PDFKit.new(File.new(@outputHtml))
   end
 
   def generate
-    @pdf.to_file("files/voucher.pdf")
-    Launchy::Browser.run("files/voucher.pdf")
+    @pdf.to_file(@outputPdf)
+    Launchy::Browser.run(@outputPdf)
+
+    File.delete(@outputHtml)
   end
 
 end
