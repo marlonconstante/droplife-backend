8d1596c24c72777a7028dd7d116250c6fca42b1b
Marlon Santos Constante <marlon.constante@gmail.com>
2014-03-26 13:28:29 -0300

    Ajuste layout voucher

--- src/ruby/experience_detail.rb
+++ src/ruby/experience_detail.rb
@@ -7,7 +7,7 @@ class ExperienceDetail
   def map(key)
     begin
       doc = Nokogiri::HTML(open("#{URL}-#{key}.html"))
-      {:termsOfUse => doc.css("dl-box[name=\"Termos de uso\"] dl-list item").map { |item| "<li>" + item.text + "</li>" }.join("").strip,
+      {:termsOfUse => doc.css("dl-box[name=\"Termos de uso\"] dl-list item").map { |item| "<li><span>" + item.text + "</span></li>" }.join("").strip,
        :local => doc.css("dl-box[name=\"LOCALIZAÇÃO\"]").map { |address| address.text }.join("").strip}.to_json
     rescue Exception => ex
       {:error => {:message => ex.message}}.to_json
